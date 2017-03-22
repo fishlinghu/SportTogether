08bf18aa026a6876ba34d9f4b75dbd45e31ef052
rueibao <rbxxxd@gmail.com>
2017-03-22 13:17:42 -0400

    Add Main page activity & layout

---
+++ app/src/main/java/fishlinghu/sporttogether/MainPageActivity.java
@@ -0,0 +1,55 @@
+package fishlinghu.sporttogether;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainPageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        Button EditButton = (Button) findViewById(R.id.Edit_Profile_button);
+        Button MatchButton = (Button) findViewById(R.id.Matching_button);
+        Button FriendButton = (Button) findViewById(R.id.Friends_button);
+
+        EditButton.setOnClickListener(this);
+        MatchButton.setOnClickListener(this);
+        FriendButton.setOnClickListener(this);
+
+        Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
+
+
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Edit_Profile_button:
+                Toast.makeText(v.getContext(), "edit", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.Matching_button:
+                Toast.makeText(v.getContext(), "match", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.Friends_button:
+                Toast.makeText(getApplicationContext(), "Friend", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(MainPageActivity.this, MainActivity.class));
+                break;
+
+        }
+    }
+
+
+
+
+
+
+}
--- app/src/main/java/fishlinghu/sporttogether/SignInActivity.java
+++ app/src/main/java/fishlinghu/sporttogether/SignInActivity.java
@@ -125,7 +125,7 @@ public class SignInActivity extends AppCompatActivity implements
                             Toast.makeText(SignInActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         } else {
-                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                            startActivity(new Intent(SignInActivity.this, MainPageActivity.class));
                             finish();
                         }
                     }
