8ccf0d243e0f82f7477c01106ee60fdbc63aa152
fishlinghu <fishlinghu@gmail.com>
2017-04-12 03:32:53 -0400

    dynamically create rating bar, but textView is not showing

--- app/src/main/java/fishlinghu/sporttogether/ReviewActivity.java
+++ app/src/main/java/fishlinghu/sporttogether/ReviewActivity.java
@@ -3,9 +3,13 @@ package fishlinghu.sporttogether;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -33,6 +37,8 @@ public class ReviewActivity extends AppCompatActivity {
 
     private String roomKey = "";
 
+    private ArrayList<String> userNameList = new ArrayList<String>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,44 +50,44 @@ public class ReviewActivity extends AppCompatActivity {
         AccountEmail = GoogleUser.getEmail();
         AccountEmailKey = AccountEmail.replace(".",",");
 
-        // get user data
-        Query query = reference.child("users").child( AccountEmailKey );
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    UserData = dataSnapshot.getValue(User.class);
-                    roomKey = UserData.getRoomKey();
+                roomKey = dataSnapshot.child("users").child(AccountEmailKey).getValue(User.class).getRoomKey();
+                for(DataSnapshot snapshot : dataSnapshot.child("chatrooms").child(roomKey).child("users").getChildren()){
+                    Log.d("reviewActivity", snapshot.getKey());
+                    if(!AccountEmailKey.equals(snapshot.getKey())){
+                        // add the name of other user to the list
+                        userNameList.add( snapshot.getKey() );
+                    }
+                }
+                LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayoutReview);
+                int numUsers = userNameList.size();
+                for(int i = 0; i < numUsers; i++) {
+                    TextView tempTextView = new TextView( getApplicationContext() );
+                    RatingBar tempRatingBar = new RatingBar( getApplicationContext() );
+
+                    // show user name here
+                    Log.d("reviewActivity", userNameList.get(i));
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    //LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    tempTextView.setText( userNameList.get(i) );
+                    //tempTextView.setText( "Fuck you" );
+                    tempTextView.setVisibility(View.VISIBLE);
+                    tempTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    ll.addView( tempTextView );
+                    //ll.addView(tempTextView, lp2);
+                    tempRatingBar.setRating(5);
+                    //ll.addView(tempRatingBar, lp);
                 }
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
         });
 
-        // get room data
-        query = reference.child("chatrooms").child(roomKey);
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    ChatroomData = dataSnapshot.getValue(Chatroom.class);
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayoutReview);
-        for(int i = 0; i < 2; i++){
-            TextView temp = new TextView(this);
-            temp.setText(String.valueOf(i));
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(100, LinearLayout.LayoutParams.WRAP_CONTENT);
-            ll.addView(temp,lp);
-        }
 
     }
 }
