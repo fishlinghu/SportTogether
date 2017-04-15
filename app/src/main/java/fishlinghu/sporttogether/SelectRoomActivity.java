427fd57fd0b78fc8ebcadf31fc97e21f52fe833c
fishlinghu <fishlinghu@gmail.com>
2017-04-15 01:47:50 -0400

    finish SelectRoomActivity, can enter the chatroom by selecting the room

--- app/src/main/java/fishlinghu/sporttogether/SelectRoomActivity.java
+++ app/src/main/java/fishlinghu/sporttogether/SelectRoomActivity.java
@@ -1,7 +1,12 @@
 package fishlinghu.sporttogether;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,6 +16,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static android.view.View.generateViewId;
 
 public class SelectRoomActivity extends AppCompatActivity {
 
@@ -34,8 +43,42 @@ public class SelectRoomActivity extends AppCompatActivity {
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                final LinearLayout ll = (LinearLayout) findViewById(R.id.llSelectRoom);
                 for(DataSnapshot snapshot : dataSnapshot.getChildren()){
-                    String tempRoomKey = snapshot.getKey();
+                    final String tempRoomKey = snapshot.getKey();
+
+                    //Chatroom chatroomData;
+                    reference.child("chatrooms").child(tempRoomKey).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Chatroom chatroomData = dataSnapshot.getValue(Chatroom.class);
+                            if(chatroomData == null)
+                                return;
+                            Button tempButton = new Button(getApplicationContext());
+                            int tempID = genID();
+                            tempButton.setId( tempID );
+                            tempButton.setText( chatroomData.getSport()
+                                    + "," + chatroomData.getIntendedTime()
+                                    + "," + chatroomData.getZipcode() );
+                            ll.addView( tempButton );
+
+                            // set onClick
+                            tempButton.setOnClickListener(new View.OnClickListener(){
+                                @Override
+                                public void onClick(View v) {
+                                    // jump to the chatroom
+                                    Intent myIntent = new Intent(SelectRoomActivity.this, ChatActivity.class);
+                                    myIntent.putExtra("roomKey", tempRoomKey);
+                                    startActivity(myIntent);
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
                 }
             }
             @Override
@@ -43,5 +86,19 @@ public class SelectRoomActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private int genID(){
+        AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+        for (;;) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF)
+                newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
     }
 }
