0b1879f4466a558909792940878a7389b5eb3b25
fishlinghu <fishlinghu@gmail.com>
2017-04-19 01:17:49 -0400

    finish friend request sending function and button in profileActivity

--- app/src/main/java/fishlinghu/sporttogether/ProfileActivity.java
+++ app/src/main/java/fishlinghu/sporttogether/ProfileActivity.java
@@ -3,11 +3,15 @@ package fishlinghu.sporttogether;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.vision.text.Text;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,6 +22,9 @@ public class ProfileActivity extends AppCompatActivity {
 
     private String userEmailKey;
     private DatabaseReference reference;
+    private FirebaseUser GoogleUser;
+    private String currentUserEmail;
+    private String currentUserEmailKey;
     private User UserData;
     private TextView tempTextView;
     private ImageView tempImageView;
@@ -28,6 +35,9 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         reference = FirebaseDatabase.getInstance().getReference();
+        GoogleUser = FirebaseAuth.getInstance().getCurrentUser();
+        currentUserEmail = GoogleUser.getEmail();
+        currentUserEmailKey = currentUserEmail.replace(".", ",");
 
         // get the room key
         Intent myIntent = getIntent();
@@ -51,6 +61,16 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+
+        Button sendRequest = (Button) findViewById(R.id.buttonSendFriendReq);
+        // set the event triggered by click
+        sendRequest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // send friend req to target user
+                reference.child("users").child(userEmailKey).child("request").child(currentUserEmailKey).setValue("");
             }
         });
     }
