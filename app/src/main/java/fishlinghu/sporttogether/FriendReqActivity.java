b9376c5395ecc294412324bb737f88b39b348c8a
fishlinghu <fishlinghu@gmail.com>
2017-04-18 23:51:19 -0400

    create related activities for friend function

---
+++ app/src/main/java/fishlinghu/sporttogether/FriendReqActivity.java
@@ -0,0 +1,12 @@
+package fishlinghu.sporttogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendReqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
